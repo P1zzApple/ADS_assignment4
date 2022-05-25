0da51dce0d073d0e99e354c48447e3ea1bfd2daa
P1zzAITU <Aidar_amankeldy2004@mail.ru>
2022-05-25 23:26:23 +0600

    Added some code to all files

--- MyDjakstraSearch.java
+++ MyDjakstraSearch.java
@@ -1,2 +1,66 @@
-public class MyDjakstraSearch {
+import java.util.*;
+
+public class MyDjakstraSearch<V> extends MySearch<V> {
+    private Set<V> unsettledNodes;
+    private Map<V, Double> distances;
+    private MyWeightedGraph<V> graph;
+
+    public MyDjakstraSearch(MyWeightedGraph<V> graph, V source) {
+        super(source);
+        unsettledNodes = new HashSet<>();
+        distances = new HashMap<>();
+        this.graph = graph;
+        djakstra();
+    }
+
+    public void djakstra() {
+        distances.put(source, 0D);
+        unsettledNodes.add(source);
+
+        while (unsettledNodes.size() > 0) {
+            V node = getVertexWithMinimumWeight(unsettledNodes);
+            marked.add((V) node);
+            unsettledNodes.remove(node);
+            for (V target : graph.adjacencyList(node)) {
+                if (getShortestDistance(target) > getShortestDistance(node)
+                        + getDistance(node, target)) {
+                    distances.put(target, getShortestDistance(node)
+                            + getDistance(node, target));
+                    edgeTo.put(target, node);
+                    unsettledNodes.add(target);
+                }
+            }
+        }
+    }
+
+    private double getDistance(V node, V target) { // OWN
+        Collection temp = graph.getWeight(node);
+        int i = 0;
+        for (MyWeightedGraph<V>.Vertex edge : graph.getEdges(node)) {
+            if (edge.data.equals(target)) {
+                return (double) temp.toArray()[i];
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    private V getVertexWithMinimumWeight(Set<V> vertices) {
+        V minimum = null;
+        for (V vertex : vertices) {
+            if (minimum == null)
+                minimum = vertex;
+            else {
+                if (getShortestDistance(vertex) < getShortestDistance(minimum))
+                    minimum = vertex;
+            }
+        }
+        return minimum;
+    }
+
+    private double getShortestDistance(V destination) {
+        Double d = distances.get(destination);
+        return (d == null ? Double.MAX_VALUE : d);
+    }
 }
--- MyVertex.java
+++ MyVertex.java
@@ -1,4 +1,5 @@
 import java.util.Map;
+import java.util.Objects;
 
 public class MyVertex<V> {
     private V data;
@@ -12,7 +13,53 @@ public class MyVertex<V> {
         this.weight = weight;
     }
 
+    public MyVertex(V data, V dest) {
+        this.data = data;
+        this.dest = dest;
+    }
+
     public void addAdjVertex(MyVertex<V> dest, double weight) {
         adjVertices.put(dest, weight);
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public void setData(V data) {
+        this.data = data;
+    }
+
+    public Map<MyVertex<V>, Double> getAdjVertices() {
+        return adjVertices;
+    }
+
+    public void setAdjVertices(Map<MyVertex<V>, Double> adjVertices) {
+        this.adjVertices = adjVertices;
+    }
+
+    public V getDest() {
+        return dest;
+    }
+
+    public void setDest(V dest) {
+        this.dest = dest;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyVertex vertex = (MyVertex) o;
+        return Objects.equals(data, vertex.data) &&
+                Objects.equals(dest, vertex.dest);
     }
 }
